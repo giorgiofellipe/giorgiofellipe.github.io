ee71105fa39eb503e79a5c2d7fad62f22020b1d9
Giorgio Fellipe <giorgiofellipe@gmail.com>
2014-08-14 21:47:53 -0300

    Added skills and experience sections; added owl carousel abd easypiechart plugins to skills section

--- js/script.js
+++ js/script.js
@@ -14,13 +14,25 @@ $(document).ready(function(){
 		play: 5000
 	});
 	
+	var owl = $("#slider-skills");
+	owl.owlCarousel({
+		autoPlay:true,
+		items:5,
+		itemsDesktop:[1000,4],
+		itemsDesktopSmall:[900,3],
+		itemsTablet:[600,2],
+		itemsMobile:[480,1]
+	});
+
 	// Animations
 	var windowH = $(window).height();
 
 	$(window).bind('resize', function () {
 		windowH = $(window).height();
 	});
-
+	
+	var skillsTopOffset = $('.skills').offset().top;
+	var timelineTopOffset = $('.experience-line').offset().top;
 	$(window).scroll(function(){
 		// Fixed Navbar
 		if(window.pageYOffset > windowH) {
@@ -30,5 +42,24 @@ $(document).ready(function(){
 			$('.navbar-flat').removeClass('navbar-fixed-top');
 			$('#about').removeClass('fixed');
 		}
+
+		if(window.pageYOffset > skillsTopOffset-windowH+200) {
+			$('.pie-graph').easyPieChart({
+				easing: 'easeInOut',
+				barColor: '#F16272',
+				trackColor: '#F7F6F6',
+				scaleColor: false,
+				lineWidth: 4,
+				size: 150,
+				onStep: function(from, to, percent) {
+					$(this.el).find('.percent').text(Math.round(percent));
+				}
+			});
+		}
+
+		if(window.pageYOffset > timelineTopOffset-windowH+200) {
+			$('.experience-line li').addClass('fadeInUp');
+		}
 	});
+
 });
